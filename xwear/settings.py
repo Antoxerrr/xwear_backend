9f2dcc79879336376abd55940c76dcbeb00bf312
Маляров Антон <malyarov_anton@internet-tariff.ru>
2025-07-07 02:19:21 +1000

    logging

--- xwear/settings.py
+++ xwear/settings.py
@@ -44,6 +44,38 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'xwear.urls'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
